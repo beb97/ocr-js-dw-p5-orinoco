b43e62bda9d4ad005749d927bd51c7700cd45161
unknown <beb97@hotmail.com>
2021-01-16 17:35:53 +0100

    ajout du style reservia page accueil et produit

--- public/js/index.js
+++ public/js/index.js
@@ -9,19 +9,16 @@
 
  function createItem(item) {
      console.log(item);
-     let tableTemplate = document.getElementById("item-table");
+     let itemTemplate = document.getElementById("item-template");
 
-     let tableClone = tableTemplate.cloneNode(true)
+     let tableClone = itemTemplate.cloneNode(true)
 
      tableClone.removeAttribute("style");
-     tableClone.getElementsByClassName("item-link")[0].innerHTML = item._id;
+//     tableClone.getElementsByClassName("item-link")[0].innerHTML = item._id;
      tableClone.getElementsByClassName("item-link")[0].setAttribute("href", generateItemLink(item._id));
      tableClone.getElementsByClassName("name")[0].innerHTML = item.name;
-     tableClone.getElementsByClassName("price")[0].innerHTML = item.price;
-//     tableClone.getElementsByClassName("desc")[0].innerHTML = item.description;
+     tableClone.getElementsByClassName("price")[0].innerHTML = formatPriceEuro(item.price);
      tableClone.getElementsByClassName("img")[0].setAttribute("src", generateImageUrl(item.imageUrl));
-     tableClone.getElementsByClassName("buttonAddToCart")[0].setAttribute("onClick", generateAddToCartLink(item._id));
-     tableClone.getElementsByClassName("buttonRemoveFromCart")[0].setAttribute("onClick", generateRemoveFromCartLink(item._id));
      document.getElementById("item-list").appendChild(tableClone);
 
  }
